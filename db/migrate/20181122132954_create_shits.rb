c0cb131dcaa1487d1c364feb5978989f778afcac
Alyson Takai <shtakai@gmail.com>
2018-11-22 20:31:30 +0700

    migrated db

--- db/migrate/20181122132954_create_shits.rb
+++ db/migrate/20181122132954_create_shits.rb
@@ -2,6 +2,7 @@ class CreateShits < ActiveRecord::Migration[5.1]
   def change
     create_table :shits do |t|
       t.string :word
+      t.references :udon
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181122132010) do
+ActiveRecord::Schema.define(version: 20181122132954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shits", force: :cascade do |t|
+    t.string "word"
+    t.bigint "udon_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["udon_id"], name: "index_shits_on_udon_id"
+  end
 
   create_table "udons", force: :cascade do |t|
     t.string "name"
